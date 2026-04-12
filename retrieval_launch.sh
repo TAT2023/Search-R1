97b65d19c2008503205e41130a39f9d098ac85f6
TAT <tongjinguang@foxmail.com>
2026-04-12 14:39:13 +0800

    update

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -3,7 +3,7 @@ file_path=/root/shared-nvme/wiki
 index_file=$file_path/e5_Flat.index
 corpus_file=$file_path/wiki-18.jsonl
 retriever_name=e5
-retriever_path=intfloat/e5-base-v2
+retriever_path=/root/shared-nvme/models/e5-base-v2
 LOG_FILE=retrieval_server.log
 
 nohup python search_r1/search/retrieval_server.py --index_path $index_file \
