3d47203304ed1e098a8d4836b6cc84543696d2ec
TAT <tongjinguang@foxmail.com>
2026-04-12 13:15:06 +0800

    update

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -4,10 +4,15 @@ index_file=$file_path/e5_Flat.index
 corpus_file=$file_path/wiki-18.jsonl
 retriever_name=e5
 retriever_path=intfloat/e5-base-v2
+LOG_FILE=retrieval_server.log
 
-python search_r1/search/retrieval_server.py --index_path $index_file \
-                                            --corpus_path $corpus_file \
-                                            --topk 3 \
-                                            --retriever_name $retriever_name \
-                                            --retriever_model $retriever_path \
-                                            --faiss_gpu
+nohup python search_r1/search/retrieval_server.py --index_path $index_file \
+                                                   --corpus_path $corpus_file \
+                                                   --topk 3 \
+                                                   --retriever_name $retriever_name \
+                                                   --retriever_model $retriever_path \
+                                                   --faiss_gpu \
+                                                   > $LOG_FILE 2>&1 &
+
+RETRIEVAL_PID=$!
+echo "Retrieval server started in background. PID=$RETRIEVAL_PID, log=$LOG_FILE"
